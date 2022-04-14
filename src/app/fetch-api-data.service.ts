0ed0c31f7eb565ca15ac16ac8ee66843cd0d9563
Matt Howell <elwhwl@gmail.com>
2022-04-14 16:32:21 -0400

    first commit

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/internal/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const apiUrl = 'https://matt-howell-myflix.herokuapp.com/movies';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserRegistrationService {
+
+  constructor(private http: HttpClient) { }
+
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http.post(apiUrl + 'users', userDetails).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred: ', error.error.message);
+    } else {
+      console.error(`Error Status code ${error.status},` + `Error body is: ${error.error}`);
+    }
+    return throwError(() => 'Something bad happened; please try again later.');
+  }
+
+}
