b4298e2d88fe192b4d9d203d7dd5a8842b29d14d
Matt Howell <elwhwl@gmail.com>
2022-05-09 14:34:20 -0400

    resolving git conflicts

--- src/app/director-view/director-view.component.ts
+++ src/app/director-view/director-view.component.ts
@@ -1,3 +1,4 @@
+/** The DirectorViewComponent renders a mat dialog containing information about the director of the movie selected */
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -10,11 +11,14 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class DirectorViewComponent implements OnInit {
 
+  /** The data that was passed to the Director dialog in the MovieCardComponent is injected in to the constructor using the MAT_DIALOG_DATA  *injection token.  The data becomes a property of the class and is available to be used in the template. 
+  */
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<DirectorViewComponent>,
     public snackBar: MatSnackBar) { }
-
+  
+    /** Positions the dialog in the upper right hand corner */
   ngOnInit() {
     this.dialogRef.updatePosition({ top: '30px', right: '30px' });
   }
--- src/app/genre-view/genre-view.component.ts
+++ src/app/genre-view/genre-view.component.ts
@@ -1,3 +1,4 @@
+/** GenreViewComponent opens a dialog that displays information about the genre of the selected movie */
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -10,11 +11,14 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class GenreViewComponent implements OnInit {
 
+  /** The data that was passed into the GenreViewComponent from the MovieCardComponent is injected into the constructor using MAT_DIALOG_DATA.  The data is then a property of GenreViewComponent and available to be displayed in the template.
+  */
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<GenreViewComponent>,
     public snackBar: MatSnackBar) { }
 
+  /** Positions the dialog in the upper left hand corner of the screen */
   ngOnInit(): void {
     this.dialogRef.updatePosition({ top: '30px', left: '30px' });
   }
